b47176e1ef0a3442d47d5514e73d5e1690941fcd
Andrea Ramírez <77802937+Andrea-gt@users.noreply.github.com>
2024-05-22 00:57:20 -0600

    feat: Second Test

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from mtf_algorithm import mtf_algorithm
 
-# Case 1
+# Question 1
 values = [0, 1, 2, 3, 4]
 requests = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+mtf = mtf_algorithm(values, requests)
+mtf.moveToFront()
+
+# Question 2
+values = [0, 1, 2, 3, 4]
+requests = [4, 3, 2, 1, 0, 1, 2, 3, 4, 3, 2, 1, 0, 1, 2, 3, 4]
 mtf = mtf_algorithm(values, requests)
 mtf.moveToFront()
